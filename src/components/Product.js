78a209ab8dd7ba3fe4686b79902816e1637e80b3
AntonCosmin215 <anton.cosmin2001@yahoo.com>
2022-03-29 16:20:53 +0300

    Update Product page

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,46 @@
 import React from 'react'
-
+import Carousel from 'react-bootstrap/Carousel'
+import '../stylesheets/Product.css';
 const Product = () => {
   //pagina unui produs
   return (
-    <div>Product</div>
+      <Carousel>
+        <Carousel.Item className="carousel">
+          <img
+              className="d-block"
+              src= {require("../images/masina.jpg")}
+              alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item className="carousel">
+          <img
+              className="d-block"
+              src= {require("../images/masina.jpg")}
+              alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item className="carousel">
+          <img
+              className="d-block"
+              src= {require("../images/masina.jpg")}
+              alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
   )
 }
 
