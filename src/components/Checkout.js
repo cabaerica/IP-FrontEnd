28516cf237c5bb8172d9329ab4e9870bb7bfb0b9
bahnarupetronel <79132145+bahnarupetronel@users.noreply.github.com>
2022-05-03 14:48:53 +0300

    checkout.js added

---
+++ src/components/Checkout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../stylesheets/checkout.css";
+
+const Checkout = () => {
+
+    return (
+        <div className={"container"}>
+            Checkout
+        </div>
+    );
+}
+
+export default Checkout
